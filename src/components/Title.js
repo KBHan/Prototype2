8ca5bbe864afafec0eb61707b50d6b10851f088a
bundaberg <bundaberg@bundabergs-MacBook-Pro.local>
2020-02-22 20:45:27 -0700

    Init with Title

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <div className="Title-main">FY21 February Remaining Balance Report</div>
+      <br></br>
+      <div className="Title-school-name">BR 0150 Oakland</div>
+      <div className="Title-fp">By Mario Mrva, Financial Partner</div>
+    </div>
+  );
+}
+
+export default Title;
